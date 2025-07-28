4e406a0f2f3257e723c55066f0298c54ecb81cd1
Shubham Patil <patilshubham7024@gmail.com>
2025-07-28 08:49:11 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ComputerOutput from './components/ComputerOutput';
+import UserInput from './components/UserInput';
 
 function App() {
+  const [userScore, setUserScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+
+  function onUserClick(userInput) {
+    
+  }
+
+  function checkWin(userInput, computerOutput) {
+    if(userInput>computerOutput) {
+      setUserScore(userScore + 1);
+      return "User";
+    } else if(computerOutput>userInput) {
+      setComputerScore(computerScore + 1);
+      return "Computer";
+    } else if(userInput == computerOutput) {
+      return "Draw";
+    }
+  }
+
   return (
     <div className="App">
       <p>Shubham</p>
+      <div><ComputerOutput /></div>
+      <div><UserInput /></div>
+      <div>User score : {userScore}</div>
+      <div>Computer score : {computerScore}</div>
     </div>
   );
 }
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,7 @@
+export default function UserInput() {
+    return <>
+        <button class="my-button">Rock</button>
+        <button class="my-button">Paper</button>
+        <button class="my-button">Scissor</button>
+    </>
+}
